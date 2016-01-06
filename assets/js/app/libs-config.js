1bcb6fe3db8ecc7362da83967cd97f11f1dae7f6
Pavel <grammka@gmail.com>
2016-01-07 01:44:13 +0300

    Update libs-config.js
    removed api keys

--- assets/js/app/libs-config.js
+++ assets/js/app/libs-config.js
@@ -4,6 +4,6 @@ $.noty.defaults.timeout = 5000;
 //$.noty.defaults.animation.open = 'animated bounceInRight';
 //$.noty.defaults.animation.close = 'animated bounceOutRight';
 
-OAuth.initialize('qfUYisKP9b8sdaHrYjGcI__WaxI');
+OAuth.initialize('xxx');
 
-L.mapbox.accessToken = 'pk.eyJ1IjoiZ3JhbW1rYSIsImEiOiJiY2NmNjM4N2IzZDk2Nzc2ODJmZGJlYjM4ZmZkNDAwMiJ9.e9mN3Fys5RmaYxPz6sOy4A';
+L.mapbox.accessToken = 'xxx';
